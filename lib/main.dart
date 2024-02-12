94ec2c963e43b5deded0820149a8f1e2dd76a63b
Ishita Kapoor <ishitakapoor12612@gmail.com>
2024-02-12 15:00:46 +0530

    dashboard done

--- lib/main.dart
+++ lib/main.dart
@@ -100,36 +100,128 @@ class _ProductScreenState extends State<ProductScreen> {
     });
   }
 
+  final TextEditingController _searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.teal,
-        title: Text('Shopping App',
-        style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.w600,
-          fontSize: 20,
-        ),),
+        title: Text(
+          'Shopping App',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+            fontSize: 20,
+          ),
+        ),
         centerTitle: true,
       ),
       body: Column(
         children: [
           Padding(
+            padding: const EdgeInsets.only(top: 20.0, left: 5, right: 5),
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  focusColor: Colors.teal,
+                  hintText: 'Search...',
+
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () => _searchController.clear(),
+                  ),
+
+                  prefixIcon: IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () {
+
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 15.0),
+            child: Container(
+              height: MediaQuery.of(context).size.height / 4,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: InkWell(
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey,),
+                            shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Image.asset("assets/images/download.jpeg"),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: InkWell(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey,),
+                            shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Image.asset(
+                            "assets/images/fresh-mango-1635589838-6060262.jpeg"),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: InkWell(
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey,),
+                            shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Image.asset(
+                            "assets/images/51ebZJ+DR4L._AC_UF1000,1000_QL80_.jpg"),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: InkWell(
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey,),
+                            shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Image.asset("assets/images/orange.jpg",),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 15),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('Explore Items',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500
-                ),),
+                Text(
+                  'Explore Items',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                ),
                 DropdownButton<String>(
                   value: _selectedCategory,
                   onChanged: (newValue) => onPressed(newValue!),
-                  items:
-                      _categories.map<DropdownMenuItem<String>>((String? category) {
+                  items: _categories
+                      .map<DropdownMenuItem<String>>((String? category) {
                     return DropdownMenuItem<String>(
                       value: category,
                       child: Text(category!),
@@ -146,10 +238,12 @@ class _ProductScreenState extends State<ProductScreen> {
               itemBuilder: (BuildContext context, int index) {
                 Product product = getFilteredProducts()[index];
                 return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
                   child: ListTile(
                     minVerticalPadding: 20,
-                    shape: RoundedRectangleBorder(side:BorderSide(width: 1, color: Colors.grey.shade300),
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(width: 1, color: Colors.grey.shade300),
                         borderRadius: BorderRadius.all(Radius.circular(5))),
                     // style: ListTileStyle,
                     leading: Image.asset(product.image.toString()),
@@ -172,7 +266,8 @@ class _ProductScreenState extends State<ProductScreen> {
                             content: TextFormField(
                               controller: _quantityController,
                               keyboardType: TextInputType.number,
-                              decoration: InputDecoration(labelText: 'Quantity'),
+                              decoration:
+                                  InputDecoration(labelText: 'Quantity'),
                             ),
                             actions: [
                               TextButton(
